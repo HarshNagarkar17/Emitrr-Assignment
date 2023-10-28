73d5b7405a345a138814987010a05390e5d241e2
Harsh Nagarkar <harshvn187@gmail.com>
2023-10-28 18:01:09 +0530

    user preference questions

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Route path="/" element={<Home/>} />
       <Route path="/login" element={<Login/>} />
       <Route path="/register" element={<Register/>} />
-      <Route path="/quiz" element={<QuizContainer/>} />
+      <Route path="/quiz/:languagePreference/:level" element={<QuizContainer/>} />
       <Route path="/profile" element={<Profile/>} />
       <Route path="/publish" element={<PublishQuestion/>} />
       <Route path="/see" element={<See/>} />
--- client/src/pages/QuizContainer.js
+++ client/src/pages/QuizContainer.js
@@ -1,9 +1,13 @@
 // src/components/QuizContainer.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { NavBar } from '../components/NavBar';
+import config from "../config/keys"
+import axios from "axios";
 import { ScoreIndicator } from '../components/ScoreIndicator';
+import { useParams, useNavigate } from "react-router-dom"
 import QuestionPalette from '../components/QuestionPalette';
+import { auth } from '../providers/auth';
 
 const QuizContainerWrapper = styled.div`
   width: 800px;
@@ -63,31 +67,61 @@ const SubmitButton = styled.button`
 `;
 
 const QuizContainer = () => {
+  const { languagePreference, level } = useParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const user = auth();
+    if (!user) navigate("/login");
+
+    axios.post(`${config.api}/who`,{}, {headers:user.headers})
+      .then((res) => {
+        const user = res.data.user;
+        if(user.languagePreference !== languagePreference) {
+          alert("This language doesn't belong to your choice!")
+          navigate("/");
+        }
+
+        const canAccessIntermediate = user.score <= 50;
+        const canAccessExperienced = user.score <= 100;
+        if(level === "intermediate" && canAccessIntermediate){
+          alert("You are not eligible for this level!")
+          navigate("/");
+        }
+
+        if(level === "experienced" && canAccessExperienced){
+          alert("You are not eligible for this level!")
+          navigate("/");
+        }
+
+      })
+    
+
+  }, [])
   return (
     <Container>
-    <NavBar/>
-    <ScoreIndicator/>
-    <QuestionPalette/>
-    <QuizContainerWrapper>
-      <Title>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto illo voluptates animi eos quibusdam voluptatem esse,
-      </Title>
-      <Options>
-        <Label>
-          <RadioInput type="radio" name="quiz-option" value="option1" /> Option 1
-        </Label>
-        <Label>
-          <RadioInput type="radio" name="quiz-option" value="option2" /> Option 2
-        </Label>
-        <Label>
-          <RadioInput type="radio" name="quiz-option" value="option3" /> Option 3
-        </Label>
-        <Label>
-          <RadioInput type="radio" name="quiz-option" value="option4" /> Option 4
-        </Label>
-      </Options>
-      <SubmitButton>Submit Answer</SubmitButton>
-    </QuizContainerWrapper>
+      <NavBar />
+      <ScoreIndicator />
+      <QuestionPalette />
+      <QuizContainerWrapper>
+        <Title>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto illo voluptates animi eos quibusdam voluptatem esse,
+        </Title>
+        <Options>
+          <Label>
+            <RadioInput type="radio" name="quiz-option" value="option1" /> Option 1
+          </Label>
+          <Label>
+            <RadioInput type="radio" name="quiz-option" value="option2" /> Option 2
+          </Label>
+          <Label>
+            <RadioInput type="radio" name="quiz-option" value="option3" /> Option 3
+          </Label>
+          <Label>
+            <RadioInput type="radio" name="quiz-option" value="option4" /> Option 4
+          </Label>
+        </Options>
+        <SubmitButton>Submit Answer</SubmitButton>
+      </QuizContainerWrapper>
     </Container>
   );
 };
